e0adf1131934e5abcf83605518795a5eab508123
fescalona4 <fescalona4@gmail.com>
2015-11-27 14:30:21 -0500

    added playlist dialog

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -45,19 +45,61 @@ app.config(['$routeProvider',
   }]);
 
 
-app.controller('appController', ['$scope', '$location','$mdSidenav',function($scope,$location,$mdSidenav) {
+app.controller('appController', ['$scope', '$location','$mdSidenav','$mdDialog',
+    function($scope,$location,$mdSidenav,$mdDialog) {
 	
 		var appCtrl = this;
+    $scope.playlistShown = false;
+    
+    //console.log('Current route name: ' + $location.path());
+    if($location.path()=="/home"){
+      $scope.title = "Dashboard";
+    }
+    else if($location.path()=="/new"){
+      $scope.title = "New Releases";
+    }
+    else if($location.path()=="/top"){
+      $scope.title = "Top Charts";
+    }
 
 		appCtrl.toggleSidenav = function(menuId) {
 			$mdSidenav(menuId).toggle();
 
 		};
 
-		$scope.go = function ( path ) {
-		  $location.path( path );
+		$scope.go = function ( path, title ) {
+		  $location.path( path ); //go to that route
+      $scope.title = title;
 		};
 
+
+    $scope.showPlaylistQueue = function(ev) {
+    
+        $scope.playlistShown = true;
+
+        $mdDialog.show({
+          //controller: DialogController,
+          templateUrl: 'partials/playlistQueue.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true,
+          disableParentScroll:false,
+          hasBackdrop:false
+        })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+          $scope.playlistShown = false;
+          
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+      
+    };
+
+    $scope.closeDialog = function() {
+        $mdDialog.hide();
+    }
+    
 
 }]);
 
