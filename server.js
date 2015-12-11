a7e93a2227c3d11d940a345e19d8ddbc2fb48ada
j-diaz <jrd.palacios@gmail.com>
2015-12-10 21:54:59 -0400

    Fixing indentatiation in server.js

--- server.js
+++ server.js
@@ -1,32 +1,27 @@
+var port 				= process.env.PORT || 8000;
+var express 		= require('express');
+var bodyParser 	= require('body-parser');
+var path 				= require('path');
+var app 				= express();
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var AWS = require('aws-sdk');
-var app = express();
+var songs 			= require('./routes/songs');
 
-
-
-//configure body-parser
+// Configure middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-var songs = require('./routes/songs');
-app.use('/api', songs); //This is our route for calling
 
-module.exports = app;
+// Collect visitor metric
+// Todo
 
+// bootstrap routes
+app.use('/api', songs); 
 
-
-// This is also a better approach for providing port
-// It says to use the port specified in the environment var of the process
-// if not provided default to 8000
-var port = process.env.PORT || 8000;
-
-// the /app path is relative to whatever the __dirname variable resolves to
+// static file serving
 var staticFileDir = __dirname + '/app';
 app.use(express.static(staticFileDir));
 app.use(express.static(__dirname));
 
+app.listen(port, function(){
+	console.log('Server started listening port: ' +port);	
+});
 
-app.listen(port);
-console.log('Listening on port: ' + port);
