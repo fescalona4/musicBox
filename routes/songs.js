71fd3e02d2f33a9d8d16321087e1ad8364d1cfdb
j-diaz <jrd.palacios@gmail.com>
2015-12-10 21:34:03 -0400

    Fixing indentation with spaces

--- routes/songs.js
+++ routes/songs.js
@@ -5,47 +5,47 @@ var db = require('../db/dbmanager.js');
 // GET service to get all existing songs in DB
 router.get('/get-all-songs', function(req, res, next) {
 
-		db.findAllSongs(function(songs){
-				res.send(songs);
-		}); 
+    db.findAllSongs(function(songs){
+        res.send(songs);
+    }); 
 });
 
 // GET service to get single song details by id
 router.get('/song/:id', function(req, res, next) {
 
-	 var id = req.params.id;
+   var id = req.params.id;
 
-	 db.findSongById(id, function(song){
-		 res.send(song);
-	 });
+   db.findSongById(id, function(song){
+     res.send(song);
+   });
 });
 
 // PUT service to increase song play-count by 1
 router.put('/song/play-count/:id', function(req, res, next) {
 
-	 var id = req.params.id;
+   var id = req.params.id;
 
-	 db.increasePlayCount(id, function(result){
-		res.send(result);
-	 });
+   db.increasePlayCount(id, function(result){
+    res.send(result);
+   });
 });
 
 // PUT service to increase song download-count by 1
 router.put('/song/download-count/:id', function(req, res, next) {
 
-	 var id = req.params.id;
+   var id = req.params.id;
 
-		db.increaseDownloadCount(id, function(result){
-		res.send(result);
-	 });
+    db.increaseDownloadCount(id, function(result){
+    res.send(result);
+   });
 });
 
 // POST service to load all songs from json to db
 router.post('/load-all-songs', function(req, res, next) {
 
-		db.loadAllFromJson(function(result){
-				res.send(result);
-		});
+    db.loadAllFromJson(function(result){
+        res.send(result);
+    });
 });
 
 module.exports = router;
