d0b3dd7fac90d4142830129ef98b4aa282bb7474
jose Diaz <jrd.palacios@gmail.com>
2015-12-10 21:31:48 -0400

    Fixing indentation

--- db/dbmanager.js
+++ db/dbmanager.js
@@ -10,106 +10,106 @@ module.exports = {
 	// Find all songs in db
 	findAllSongs: function(callback){
 		// format
-	  var params = {
-      TableName: 'Songs'
-    };
+		var params = {
+			TableName: 'Songs'
+		};
 
-    console.log('Finding all songs...');
+		console.log('Finding all songs...');
 
 
-  	dynamodbDoc.scan(params, function(err, data) {
-	  	// Retrieve the json data in dynamo db
-	  	var songs = [];
-      if (err) {
-        	// An error occurred
-          console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-          songs = err;
-      } 
-      else {
-          // Successful response
-          console.log("api/get-all-songs");
-          songs = data.Items;
-      }
-      callback(songs);
-	  });
+		dynamodbDoc.scan(params, function(err, data) {
+			// Retrieve the json data in dynamo db
+			var songs = [];
+			if (err) {
+					// An error occurred
+					console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+					songs = err;
+			} 
+			else {
+					// Successful response
+					console.log("api/get-all-songs");
+					songs = data.Items;
+			}
+			callback(songs);
+		});
 	},
 
 	findSongById: function(id, callback){
 
 		var params = {
-      TableName: 'Songs',
-      Key: {
-          "id":  parseInt(id)
-      }
-    };
+			TableName: 'Songs',
+			Key: {
+					"id":  parseInt(id)
+			}
+		};
 
-    return dynamodbDoc.get(params, function(err, data) {
-  	  var song = {};
-      if (err) {// an error occurred
-          song = err;
-      } 
-      else {
-          // successful response
-          song = data.Item;
-      }
-      console.log(song);
-      callback(song);
-    });
+		return dynamodbDoc.get(params, function(err, data) {
+			var song = {};
+			if (err) {// an error occurred
+					song = err;
+			} 
+			else {
+					// successful response
+					song = data.Item;
+			}
+			console.log(song);
+			callback(song);
+		});
 	},
 
 	increasePlayCount: function(id, callback){
 
 	 var params = {
-      TableName: 'Songs',
-      Key: {
-          "id": parseInt(id)
-      },
-      UpdateExpression: "set playCount = playCount + :val",
-      ExpressionAttributeValues: {
-          ":val": 1
-      },
-      ReturnValues: "UPDATED_NEW"
-    };
+			TableName: 'Songs',
+			Key: {
+					"id": parseInt(id)
+			},
+			UpdateExpression: "set playCount = playCount + :val",
+			ExpressionAttributeValues: {
+					":val": 1
+			},
+			ReturnValues: "UPDATED_NEW"
+		};
 
-    dynamodbDoc.update(params, function(err, data) {
-    	  var signal = {};
-        if (err) {
-            console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
-            signal = err;
-        } else {
-            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-            signal = err;
-        }
-        callback(signal);
-    });
+		dynamodbDoc.update(params, function(err, data) {
+				var signal = {};
+				if (err) {
+						console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+						signal = err;
+				} else {
+						console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+						signal = err;
+				}
+				callback(signal);
+		});
 	},
 
 	increaseDownloadCount: function(id, callback){
 
 		var params = {
-      TableName: 'Songs',
-      Key: {
-          "id": parseInt(id)
-      },
-      UpdateExpression: "set downloadCount = downloadCount + :val",
-      ExpressionAttributeValues: {
-          ":val": 1
-      },
-      ReturnValues: "UPDATED_NEW"
-    };
+			TableName: 'Songs',
+			Key: {
+					"id": parseInt(id)
+			},
+			UpdateExpression: "set downloadCount = downloadCount + :val",
+			ExpressionAttributeValues: {
+					":val": 1
+			},
+			ReturnValues: "UPDATED_NEW"
+		};
 
-    dynamodbDoc.update(params, function(err, data) {
-    	// 
-    	var signal = {};
-      if (err) {
-          console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
-          signal = err;
-      } else {
-          console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
-          signal= err;
-      }
-      callback(signal);
-    });
+		dynamodbDoc.update(params, function(err, data) {
+			// 
+			var signal = {};
+			if (err) {
+					console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+					signal = err;
+			} else {
+					console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+					signal= err;
+			}
+			callback(signal);
+		});
 	},
 
 	// POST service to load all songs from json to db
@@ -117,37 +117,37 @@ module.exports = {
 		fs.readFile('app/myDataJson.json', 'utf8', function(){
 			
 			allMovies.forEach(function(movie) {
-	    
-		    var params = {
-		        TableName: "Songs",
-		        Item: {
-	            "id":  movie.id,
-	            "title": movie.title,
-	            "image":  movie.image,
-	            "artist":  movie.artist,
-	            "filter": movie.filter,
-	            "filename":  movie.filename,
-	            "url":  movie.url,
-	            "comments": movie.comments,
-	            "downloadCount":  movie.downloadCount,
-	            "playCount":  movie.playCount,
-	            "rating":  movie.rating
-				    }
+			
+				var params = {
+						TableName: "Songs",
+						Item: {
+							"id":  movie.id,
+							"title": movie.title,
+							"image":  movie.image,
+							"artist":  movie.artist,
+							"filter": movie.filter,
+							"filename":  movie.filename,
+							"url":  movie.url,
+							"comments": movie.comments,
+							"downloadCount":  movie.downloadCount,
+							"playCount":  movie.playCount,
+							"rating":  movie.rating
+						}
 				 };
 
-		   	dynamodbDoc.put(params, function(err, data) {
-	        var output = {};
-	        if (err) {
-	            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-	            output = err;
-	        } else {
-	            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-	            output = data;
-	        }
-	        callback(output);
-	   	 }); //ends dynamodb
+				dynamodbDoc.put(params, function(err, data) {
+					var output = {};
+					if (err) {
+							console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+							output = err;
+					} else {
+							console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+							output = data;
+					}
+					callback(output);
+			 }); //ends dynamodb
 
-    	}); //ends foreach
+			}); //ends foreach
 
 		}); // ends fsReadFile
 		
