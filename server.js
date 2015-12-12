4ac4115aac9deb9043071d175951c658aaa958f1
Francisco Escalona <fescalona4@gmail.com>
2015-12-11 21:00:40 -0500

    changing back to 8000

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var port        = process.env.PORT || 80;
+var port        = process.env.PORT || 8000;
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var path        = require('path');
