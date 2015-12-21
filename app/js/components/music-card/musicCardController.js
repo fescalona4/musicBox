d22c76da7616897a67a5d8ba4e85bfd9d0bf6cfb
Francisco Escalona <fescalona4@gmail.com>
2015-12-21 17:45:32 -0500

    new style on cards

---
+++ app/js/components/music-card/musicCardController.js
@@ -0,0 +1,14 @@
+/**/
+angular.module('musicBoxApp')
+    .controller('musicCardController', ['$scope', function($scope) {
+
+
+        var originatorEv;
+        this.openMenu = function($mdOpenMenu, ev) {
+            console.log('here');
+            originatorEv = ev;
+            $mdOpenMenu(ev);
+        };
+
+
+    }]);
--- app/js/components/music-card/musicCardDirective.js
+++ app/js/components/music-card/musicCardDirective.js
@@ -5,7 +5,9 @@ angular.module('musicBoxApp')
     .directive('musicCard', function() {
     return {
         restrict: 'E',
-        templateUrl: 'js/components/music-card/musicCardView.html'
+        templateUrl: 'js/components/music-card/musicCardView.html',
+        controller: 'musicCardController',
+        controllerAs: 'ctrl'
     };
 });
 
